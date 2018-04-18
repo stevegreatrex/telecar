dd7dfac2a5cc55201332fe6bdbaf75db2723bbc1
Steve Greatrex <stephen.greatrex@trayport.com>
2018-04-18 23:25:33 +0100

    debug

--- client.ts
+++ client.ts
@@ -7,54 +7,58 @@ let touching = false;
 let touchX: number;
 let touchY: number;
 
-inputSink.addEventListener('touchstart', e => {
-  if (!connected) return;
-  const touchEvent = e as TouchEvent;
-  touching = true;
-  touchX = touchEvent.touches[0].clientX;
-  touchY = touchEvent.touches[0].clientY;
-});
+try {
+  inputSink.addEventListener('touchstart', e => {
+    if (!connected) return;
+    const touchEvent = e as TouchEvent;
+    touching = true;
+    touchX = touchEvent.touches[0].clientX;
+    touchY = touchEvent.touches[0].clientY;
+  });
 
-inputSink.addEventListener('touchmove', e => {
-  if (!connected) return;
-  if (!touching) return;
+  inputSink.addEventListener('touchmove', e => {
+    if (!connected) return;
+    if (!touching) return;
 
-  const touchEvent = e as TouchEvent;
-  const newX = touchEvent.touches[0].clientX;
-  const newY = touchEvent.touches[0].clientY;
+    const touchEvent = e as TouchEvent;
+    const newX = touchEvent.touches[0].clientX;
+    const newY = touchEvent.touches[0].clientY;
 
-  const yDirection = newY < touchY ? 'forward' : 'back';
-  const xOffset = Math.abs(newX - touchX);
-  let xDirection: XDirection = null;
-  if (xOffset > 30) xDirection = newX < touchX ? 'left' : 'right';
-  setDirection(yDirection, xDirection);
-  const size = Math.max(Math.max(Math.abs(newY - touchY), xOffset) * 4, 50);
-  arrow.setAttribute('style', `font-size: ${size}%`);
-});
+    const yDirection = newY < touchY ? 'forward' : 'back';
+    const xOffset = Math.abs(newX - touchX);
+    let xDirection: XDirection = null;
+    if (xOffset > 30) xDirection = newX < touchX ? 'left' : 'right';
+    setDirection(yDirection, xDirection);
+    const size = Math.max(Math.max(Math.abs(newY - touchY), xOffset) * 4, 50);
+    arrow.setAttribute('style', `font-size: ${size}%`);
+  });
 
-inputSink.addEventListener('touchend', e => {
-  if (!connected) return;
-  touching = false;
-  clearDirection();
-});
+  inputSink.addEventListener('touchend', e => {
+    if (!connected) return;
+    touching = false;
+    clearDirection();
+  });
 
-type YDirection = 'forward' | 'back';
-type XDirection = 'left' | 'right' | null;
+  type YDirection = 'forward' | 'back';
+  type XDirection = 'left' | 'right' | null;
 
-function setDirection(y: YDirection, x: XDirection) {
-  const description = `${y} ${x || ''}`;
-  log.innerHTML = description;
-  arrow.setAttribute('class', `arrow visible ${y} ${x}`);
-  socket.send(description);
+  function setDirection(y: YDirection, x: XDirection) {
+    const description = `${y} ${x || ''}`;
+    log.innerHTML = description;
+    arrow.setAttribute('class', `arrow visible ${y} ${x}`);
+    socket.send(description);
+  }
+
+  function clearDirection() {
+    log.innerHTML = '';
+    arrow.setAttribute('class', 'arrow');
+  }
+
+  const socket = new WebSocket('ws://localhost:8090');
+  socket.onopen = () => {
+    log.innerHTML = 'Connected';
+    connected = true;
+  };
+} catch (err) {
+  log.innerHTML = JSON.stringify(err, null, 2);
 }
-
-function clearDirection() {
-  log.innerHTML = '';
-  arrow.setAttribute('class', 'arrow');
-}
-
-const socket = new WebSocket('ws://localhost:8090');
-socket.onopen = () => {
-  log.innerHTML = 'Connected';
-  connected = true;
-};
--- dist/client.js
+++ dist/client.js
@@ -6,49 +6,54 @@ let connected = false;
 let touching = false;
 let touchX;
 let touchY;
-inputSink.addEventListener('touchstart', e => {
-    if (!connected)
-        return;
-    const touchEvent = e;
-    touching = true;
-    touchX = touchEvent.touches[0].clientX;
-    touchY = touchEvent.touches[0].clientY;
-});
-inputSink.addEventListener('touchmove', e => {
-    if (!connected)
-        return;
-    if (!touching)
-        return;
-    const touchEvent = e;
-    const newX = touchEvent.touches[0].clientX;
-    const newY = touchEvent.touches[0].clientY;
-    const yDirection = newY < touchY ? 'forward' : 'back';
-    const xOffset = Math.abs(newX - touchX);
-    let xDirection = null;
-    if (xOffset > 30)
-        xDirection = newX < touchX ? 'left' : 'right';
-    setDirection(yDirection, xDirection);
-    const size = Math.max(Math.max(Math.abs(newY - touchY), xOffset) * 4, 50);
-    arrow.setAttribute('style', `font-size: ${size}%`);
-});
-inputSink.addEventListener('touchend', e => {
-    if (!connected)
-        return;
-    touching = false;
-    clearDirection();
-});
-function setDirection(y, x) {
-    const description = `${y} ${x || ''}`;
-    log.innerHTML = description;
-    arrow.setAttribute('class', `arrow visible ${y} ${x}`);
-    socket.send(description);
+try {
+    inputSink.addEventListener('touchstart', e => {
+        if (!connected)
+            return;
+        const touchEvent = e;
+        touching = true;
+        touchX = touchEvent.touches[0].clientX;
+        touchY = touchEvent.touches[0].clientY;
+    });
+    inputSink.addEventListener('touchmove', e => {
+        if (!connected)
+            return;
+        if (!touching)
+            return;
+        const touchEvent = e;
+        const newX = touchEvent.touches[0].clientX;
+        const newY = touchEvent.touches[0].clientY;
+        const yDirection = newY < touchY ? 'forward' : 'back';
+        const xOffset = Math.abs(newX - touchX);
+        let xDirection = null;
+        if (xOffset > 30)
+            xDirection = newX < touchX ? 'left' : 'right';
+        setDirection(yDirection, xDirection);
+        const size = Math.max(Math.max(Math.abs(newY - touchY), xOffset) * 4, 50);
+        arrow.setAttribute('style', `font-size: ${size}%`);
+    });
+    inputSink.addEventListener('touchend', e => {
+        if (!connected)
+            return;
+        touching = false;
+        clearDirection();
+    });
+    function setDirection(y, x) {
+        const description = `${y} ${x || ''}`;
+        log.innerHTML = description;
+        arrow.setAttribute('class', `arrow visible ${y} ${x}`);
+        socket.send(description);
+    }
+    function clearDirection() {
+        log.innerHTML = '';
+        arrow.setAttribute('class', 'arrow');
+    }
+    const socket = new WebSocket('ws://localhost:8090');
+    socket.onopen = () => {
+        log.innerHTML = 'Connected';
+        connected = true;
+    };
 }
-function clearDirection() {
-    log.innerHTML = '';
-    arrow.setAttribute('class', 'arrow');
+catch (err) {
+    log.innerHTML = JSON.stringify(err, null, 2);
 }
-const socket = new WebSocket('ws://localhost:8090');
-socket.onopen = () => {
-    log.innerHTML = 'Connected';
-    connected = true;
-};
