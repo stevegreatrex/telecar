8845e88de6ba75dcce257f9e3f3ab5be5cd8956e
Steve Greatrex <stephen.greatrex@trayport.com>
2018-04-18 23:59:06 +0100

    Ok, i guess we should use the actual server address

--- client.ts
+++ client.ts
@@ -54,7 +54,7 @@ try {
     arrow.setAttribute('class', 'arrow');
   }
 
-  const socket = new WebSocket('ws://localhost:8090');
+  const socket = new WebSocket(`ws://${window.location.host}:8090`);
   socket.onopen = () => {
     log('Connected');
     connected = true;
